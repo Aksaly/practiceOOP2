e5d3217999e678e9884966d1de2073cfa30b2f1d
Aksaly <148691705+Aksaly@users.noreply.github.com>
2024-12-20 15:57:45 +0000

    Видалено класс Employee та перейменовано файл на Payroll.java

--- Payroll.java
+++ Payroll.java
@@ -178,32 +178,6 @@ class FitnessCenterProgram {
     }
 }
 
-class Employee {
-    private String name;
-    private String employeeId;
-
-    public Employee(String name, String employeeId) {
-        this.name = name;
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-}
-
 public class Payroll {
     public static void main(String[] args) {
         // Example usage
@@ -215,7 +189,7 @@ public class Payroll {
         client.recordExercise("Push Ups");
         client.sendProgressToTrainer();
         trainer.leadTrainingSession("Yoga");
-        
+
         FitnessCenter fitnessCenter = new FitnessCenter("Main Street Gym", 50.0);
         fitnessCenter.requestTrainingSession(trainer, "Yoga");
     }
